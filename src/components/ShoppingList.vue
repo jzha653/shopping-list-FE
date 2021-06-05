fc780d9b3dfd0ba23533b4a8b8a19dc7f2a86268
jzha653 <jzha653@aucklanduni.ac.nz>
2021-06-05 14:39:39 +1200

    add initial FE for shopping list

--- src/components/ShoppingList.vue
+++ src/components/ShoppingList.vue
@@ -9,7 +9,9 @@
         <th>Actions</th>
       </tr>
       </thead>
-      <tr v-for="(item, index) in itemsList">
+      <tbody>
+
+      <tr v-for="(item, index) in itemsList" v-bind:key="item">
         <td>
           <span v-show="!item.inEditMode">{{ item.quantity }}</span>
           <input type="number" v-bind:placeholder="item.quantity" v-show="item.inEditMode" v-model="item.quantity"/>
@@ -19,62 +21,126 @@
           <input v-bind:placeholder="item.itemName" v-show="item.inEditMode" v-model="item.itemName"/>
         </td>
         <td>
-          <button type="button" class="btn btn-success" v-show="item.inEditMode" @click="editItemComplete(item)"><i
-            class="fa fa-save"></i> Save
-          </button>
-          <button type="button" class="btn btn-info" v-show="!item.inEditMode" @click="editItem(item)"><i
-            class="fa fa-edit"></i> Edit
-          </button>
-          <button type="button" class="btn btn-danger" @click="removeItem(index)"><i class="fa fa-remove"></i> Delete
-          </button>
+          <Button icon="pi pi-check" class=" p-button-rounded p-button-success p-button-outlined" type="button"
+                  v-show="item.inEditMode" @click="editItemComplete(item)"/>
+          <Button icon="pi pi-user-edit" class="p-button-rounded p-button-secondary p-button-outlined" type="button"
+                  v-show="!item.inEditMode" @click="editItem(item)"/>
+          <Button icon="pi pi-times" class="p-button-rounded p-button-danger p-button-outlined"
+                  style="margin-left: 0.6rem" type="button"
+                  @click="removeItem(index)"/>
         </td>
       </tr>
+      </tbody>
     </table>
     <h4>Add new item</h4>
-    <div class="row col-md-6">
-      <div class="col-md-6 form-group">
+    <div class="p-grid" style="width: 50%;    margin-left: auto;
+    margin-right: auto;">
+      <div class="p-col-2 p-sm-6" style="width: 5rem">
         Quantity
+      </div>
+      <div class="p-col-3 p-sm-6" style="width: calc(50% - 5rem)">
         <input type="number" v-model="quantity" class="checkbox" autofocus>
       </div>
-      <div class="col-md-6 form-group">
+      <div class="p-col-3 p-sm-6" style="width: 5rem">
         Name
+      </div>
+      <div class="p-col-3 p-sm-6" style="width: calc(50% - 5rem)">
         <input type="text" v-model="itemName" class="checkbox">
       </div>
-
-      <button type="button" @click="addItem" class="btn btn-primary"><i class="fa fa-plus"></i> Add</button>
+    </div>
+    <div>
+      <Button label="Submit" @click="addItem" icon="pi pi-check" iconPos="left" style="margin-top: 1rem"/>
     </div>
   </form>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
 
+export default {
+  name: 'ShoppingList',
+  props: {},
+  data () {
+    return {
+      quantity: '',
+      itemName: '',
+      itemsList: [
+        {
+          quantity: 3,
+          itemName: 'Apples',
+          inEditMode: false
+        },
+        {
+          quantity: 6,
+          itemName: 'Pears',
+          inEditMode: false
+
+        }],
+      inEditMode: false
+    }
+  },
+  methods: {
+    addItem: function () {
+      var quantityIN = this.quantity
+      var itemNameIN = this.itemName.trim()
+      this.itemsList.push({
+        quantity: quantityIN,
+        itemName: itemNameIN,
+        inEditMode: false
+      })
+      this.clearAll()
+    },
+    clearQuantity: function () {
+      this.quantity = ''
+    },
+    clearItemName: function () {
+      this.itemName = ''
+    },
+    clearAll: function () {
+      this.clearQuantity()
+      this.clearItemName()
+    },
+    removeItem: function (index) {
+      this.itemsList.splice(index, 1)
+    },
+    editItem: function (item) {
+      item.inEditMode = true
+    },
+    editItemComplete: function (item) {
+      item.inEditMode = false
+    }
+  }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-body {
+form {
   padding: 1%;
-  background-color: #abca
+}
+
+input {
+  width: 100%;
+}
+
+table {
+  margin-left: auto;
+  margin-right: auto;
 }
 
 h2, h4 {
   font-family: 'Nunito', sans-serif;
 }
 
+td {
+  text-align: center;
+}
 
 #shopping-list-table {
-  table-layout: fixed;
   width: 50%;
   vertical-align: middle;
 }
 
-button {
-  margin-left: 2%;
+#shopping-list {
+  text-align: center;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config'
+import Dialog from 'primevue/dialog'
+import Button from 'primevue/button'
+import 'primevue/resources/themes/saga-blue/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(PrimeVue)
+
+app.component('Dialog', Dialog)
+app.component('Button', Button)
+
+app.mount('#app')
