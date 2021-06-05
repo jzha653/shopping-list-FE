c5b2fdfa56032c409b2c7845a05b18d3f7c3a2fb
jzha653 <jzha653@aucklanduni.ac.nz>
2021-06-05 13:04:26 +1200

    first commit

---
+++ src/components/ShoppingList.vue
@@ -0,0 +1,80 @@
+<template>
+  <form id="shopping-list">
+    <h2>Shopping List</h2>
+    <table id="shopping-list-table" class="table table-condensed table-hover">
+      <thead>
+      <tr>
+        <th>Quantity</th>
+        <th>Item</th>
+        <th>Actions</th>
+      </tr>
+      </thead>
+      <tr v-for="(item, index) in itemsList">
+        <td>
+          <span v-show="!item.inEditMode">{{ item.quantity }}</span>
+          <input type="number" v-bind:placeholder="item.quantity" v-show="item.inEditMode" v-model="item.quantity"/>
+        </td>
+        <td>
+          <span v-show="!item.inEditMode">{{ item.itemName }}</span>
+          <input v-bind:placeholder="item.itemName" v-show="item.inEditMode" v-model="item.itemName"/>
+        </td>
+        <td>
+          <button type="button" class="btn btn-success" v-show="item.inEditMode" @click="editItemComplete(item)"><i
+            class="fa fa-save"></i> Save
+          </button>
+          <button type="button" class="btn btn-info" v-show="!item.inEditMode" @click="editItem(item)"><i
+            class="fa fa-edit"></i> Edit
+          </button>
+          <button type="button" class="btn btn-danger" @click="removeItem(index)"><i class="fa fa-remove"></i> Delete
+          </button>
+        </td>
+      </tr>
+    </table>
+    <h4>Add new item</h4>
+    <div class="row col-md-6">
+      <div class="col-md-6 form-group">
+        Quantity
+        <input type="number" v-model="quantity" class="checkbox" autofocus>
+      </div>
+      <div class="col-md-6 form-group">
+        Name
+        <input type="text" v-model="itemName" class="checkbox">
+      </div>
+
+      <button type="button" @click="addItem" class="btn btn-primary"><i class="fa fa-plus"></i> Add</button>
+    </div>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+body {
+  padding: 1%;
+  background-color: #abca
+}
+
+h2, h4 {
+  font-family: 'Nunito', sans-serif;
+}
+
+
+#shopping-list-table {
+  table-layout: fixed;
+  width: 50%;
+  vertical-align: middle;
+}
+
+button {
+  margin-left: 2%;
+}
+</style>
