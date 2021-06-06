51a410ca3e118c8d11a17056ac6ab2841cff115a
jzha653 <jzha653@aucklanduni.ac.nz>
2021-06-06 21:55:10 +1200

    finished service integration

--- src/main.js
+++ src/main.js
@@ -7,11 +7,13 @@ import 'primevue/resources/themes/saga-blue/theme.css'
 import 'primevue/resources/primevue.min.css'
 import 'primeicons/primeicons.css'
 import 'primeflex/primeflex.css'
+import ProgressSpinner from 'primevue/progressspinner'
 
 const app = createApp(App)
 app.use(PrimeVue)
 
 app.component('Dialog', Dialog)
 app.component('Button', Button)
+app.component('ProgressSpinner', ProgressSpinner)
 
 app.mount('#app')
